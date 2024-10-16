6f93590ead6ddc274d475960c346246d58527c10
Elected21 <spushkov@yahoo.com>
2024-10-16 18:09:51 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+name = 'Sergey'
+print('Имя:', name)
+
+age = 49
+print ('Возраст:', age)
+
+new_age = 1
+print('Через год будет:', age + new_age)
+
+is_student = True
+print ('Вы студент:',  is_student)
